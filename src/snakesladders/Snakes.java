6c61a9f62e144ae4b8a40c5b8bb928fd5ff73661
DurgeshGupt <durgeshgu106@gmail.com>
2022-05-17 18:07:08 +0530

    ADDED:the position is change by throwing dice

--- src/snakesladders/Snakes.java
+++ src/snakesladders/Snakes.java
@@ -1,14 +1,58 @@
 package snakesladders;
+import java.util.Random;
 
 public class Snakes {
     public static void main(String[] args)
     {
 
-        Random dice = new Random();
+        int currPosition = 0;
+        int ladder = 2;
+        int snake = 3;
 
-        int number = 1 + dice.nextInt(6);
 
-        System.out.println("The dice number is "+ number );
+        Random rand = new Random();
+
+        int diceValue = 1 + rand.nextInt(6);
+
+        int option = 1 + rand.nextInt(3);
+
+        switch(option)
+        {
+            case 1:
+                System.out.println("player stay in same position");
+                currPosition = 0 + currPosition;
+                System.out.println("the position is " + currPosition);
+                break;
+
+
+            case 2:
+                System.out.println("player moves ahead by number");
+
+                if((diceValue + currPosition) > 100)
+                {
+                    currPosition = currPosition - diceValue;
+                    System.out.println("the position is " + currPosition);
+                }else {
+                    currPosition = currPosition + diceValue;
+                    System.out.println("the currentposition is  " + currPosition);
+                }
+
+                break;
+
+
+            case 3:
+                System.out.println("player moves behind by number");
+                currPosition = currPosition - diceValue;
+                if(currPosition<= 0) {
+                    currPosition = 0;
+                    System.out.println("the position is " + currPosition);
+                }
+                else
+                    System.out.println("the position is " + currPosition);
+
+
+        }
+
 
     }
 }
