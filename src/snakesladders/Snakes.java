aacc550ffa45e71e621b03f78f99da9abfb00ca3
DurgeshGupt <durgeshgu106@gmail.com>
2022-05-17 17:45:55 +0530

    ADDED: The position is initialized

---
+++ src/snakesladders/Snakes.java
@@ -0,0 +1,11 @@
+package snakesladders;
+
+public class Snakes {
+    public static void main(String[] args)
+    {
+        System.out.println("welcome to snake and ladder");
+        int start_Position = 0;
+
+    }
+
+}
